bc51cb5e7eed9500309c0c01f61438f37b7fd2d1
kaasym1 <kasymmuhammaduulu@gmail.com>
2024-07-14 17:54:37 +0600

    hw1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        String myFriend;
+        final int NUM = 19;
+        String word = "friend";
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        myFriend = "Islam";
+
+        System.out.println(myFriend);
+        System.out.println(NUM);
+        System.out.println(word);
+
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили ноль");
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя");
+        String name = scanner.next();
+        System.out.println("Привет," + name);
     }
 }
